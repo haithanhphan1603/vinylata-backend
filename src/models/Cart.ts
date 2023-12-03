1b9ec2cd751939858f0a3426f7909329fb33cc36
HaiPhan <haithanhphan163@gmail.com>
2023-12-03 22:39:16 +0700

    update Cart Model

---
+++ src/models/Cart.ts
@@ -0,0 +1,36 @@
+import Product from "./Product";
+import mongoose, { Schema, Types } from "mongoose";
+
+interface CartItem {
+  product: Product["_id"];
+  quantity: number;
+}
+
+export default interface Cart extends mongoose.Document {
+  items: CartItem[];
+  user: Types.ObjectId;
+  createdAt: Date;
+}
+
+const cartSchema = new Schema({
+  items: [
+    {
+      product: {
+        type: Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+      quantity: {
+        type: Schema.Types.Number,
+        default: 1,
+      },
+    },
+  ],
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+const CartModel = mongoose.model<Cart>("Cart", cartSchema, "carts");
